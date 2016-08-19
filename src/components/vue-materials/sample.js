2152dac630063da0c1217e14420202d2f4275f3d
elzup <guild0105@gmail.com>
2016-08-20 05:13:58 +0900

    Refactor: remove dusts

--- src/components/vue-materials/sample.js
+++ src/components/vue-materials/sample.js
@@ -3,12 +3,6 @@ var Vue = require('vue');
 var app = new Vue({
   el : '#app',
   components: {
-    'my-hoge': {
-      template: require('./template-hoge.html')
-    },
-    'my-foo': {
-      template: require('./template-foo.html')
-    },
     'menu-item': {
       props: ['name', 'href'],
       template: require('./template-menuitem.html')
