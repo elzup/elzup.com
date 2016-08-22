35d93eb3b3514dc8d567f5ba002350988725f5fe
elzup <guild0105@gmail.com>
2016-08-22 15:04:05 +0900

    Refactor format codes

--- src/components/product/product.jsx
+++ src/components/product/product.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 const Type = React.PropTypes
 import style from './product.css'
-import { ProductFooter} from '../product-footer/product-footer.jsx'
+import {ProductFooter} from '../product-footer/product-footer.jsx'
 
 export class Product extends React.Component {
   constructor(props) {
@@ -13,19 +13,22 @@ export class Product extends React.Component {
     let title;
     const base_title = <h2>{this.props.title}</h2>;
     if (this.props.is_alive) {
-      title = <a href={this.props.link} className={style.title_link} target="_blank">{base_title}</a>
+      title = <a href={this.props.link} className={style.title_link}
+                 target="_blank">{base_title}</a>
     } else {
       title = base_title;
     }
     const members = this.props.members.map(x => {
       const link = "//twitter.com/" + x.name
-      return <li key={this.props.sid + ":" + x.name}><a href={link}>@{x.name}</a>: {x.description}</li>
+      return <li key={this.props.sid + ":" + x.name}><a
+        href={link}>@{x.name}</a>: {x.description}</li>
     })
-    const tags = this.props.tags.map(x => <div key={this.props.sid + ":" + x}>{x}</div>)
+    const tags = this.props.tags.map(x => <div
+      key={this.props.sid + ":" + x}>{x}</div>)
     return (
       <li className={style.product}>
         <div className={style.img_wrap}>
-          <img src={img_url} />
+          <img src={img_url}/>
         </div>
         { title }
         <p>{this.props.subtitle}</p>
@@ -49,17 +52,17 @@ export class Product extends React.Component {
 }
 
 Product.propTypes = {
-  sid:  Type.string.isRequired,
-  level:  Type.number.isRequired,
-  category:  Type.string.isRequired,
-  title:  Type.string.isRequired,
-  subtitle:  Type.string.isRequired,
-  description:  Type.string.isRequired,
-  term:  Type.string.isRequired,
-  tags:  Type.array.isRequired,
-  is_alive:  Type.bool.isRequired,
-  link:  Type.string,
-  github:  Type.string,
-  trello:  Type.string,
-  members:  Type.array
+  sid: Type.string.isRequired,
+  level: Type.number.isRequired,
+  category: Type.string.isRequired,
+  title: Type.string.isRequired,
+  subtitle: Type.string.isRequired,
+  description: Type.string.isRequired,
+  term: Type.string.isRequired,
+  tags: Type.array.isRequired,
+  is_alive: Type.bool.isRequired,
+  link: Type.string,
+  github: Type.string,
+  trello: Type.string,
+  members: Type.array
 }
