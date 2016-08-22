c14acdd705d33cb5189d678be47febe0afebc924
elzup <guild0105@gmail.com>
2016-08-22 15:02:18 +0900

    Split Product footer link component

---
+++ src/components/product-footer-link/product-footer-link.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+const Type = React.PropTypes
+import style from './product-footer-link.css'
+
+export class ProductFooterLink extends React.Component {
+  render() {
+    if (this.props.url == undefined) {
+      return <span></span>
+    }
+    if (this.props.type == 'link') {
+      if (this.props.is_alive != undefined && this.props.is_alive) {
+        return (
+          <a href={this.props.url} className={style.link} target="_blank">
+            <span>↗</span>
+          </a>)
+      } else {
+        return (<span className={style.broken_link} target="_blank">
+            <span data-tip="SITE CLOSED">↗</span>
+          </span>)
+      }
+    }
+    const link_style = {
+      'github': style.link_github,
+      'trello': style.link_trello
+    }[this.props.type]
+    return (
+      <a href={this.props.url} className={link_style}
+         target="_blank">
+        <img src={"/images/icon/mk-" + this.props.type + ".png"} alt=""/>
+      </a>)
+  }
+}
+
+ProductFooterLink.propTypes = {
+  is_alive: Type.bool,
+  url: Type.string,
+  type: Type.string
+}
--- src/components/product-footer/product-footer.jsx
+++ src/components/product-footer/product-footer.jsx
@@ -2,43 +2,20 @@ import React from 'react'
 const Type = React.PropTypes
 import style from './product-footer.css'
 import ReactTooltip from 'react-tooltip'
+import {ProductFooterLink} from '../product-footer-link/product-footer-link.jsx'
 
 export class ProductFooter extends React.Component {
   render() {
-    const links = [];
     let tooltip = '';
-    if (this.props.link != undefined) {
-      if (this.props.is_alive) {
-        links.push(
-          <a key={1} href={this.props.link} className={style.link}
-             target="_blank">
-            <span>↗</span>
-          </a>)
-      } else {
-        tooltip = <ReactTooltip />
-        links.push(
-          <span key={1} className={style.broken_link} target="_blank">
-            <span data-tip="SITE CLOSED">↗</span>
-          </span>)
-      }
-    }
-    if (this.props.github != undefined) {
-      links.push(
-        <a key={2} href={this.props.github} className={style.link_github}
-           target="_blank">
-          <img src="/images/icon/mk-github.png" alt=""/>
-        </a>)
-    }
-    if (this.props.trello != undefined) {
-      links.push(
-        <a key={3} href={this.props.trello} className={style.link_trello}
-           target="_blank">
-          <img src="/images/icon/mk-trello.png" alt=""/>
-        </a>)
+    if (!this.props.is_alive) {
+      tooltip = <ReactTooltip />
     }
     return (
       <div className={style.footer}>
-        {links}
+        <ProductFooterLink is_broken={this.props.is_alive} type={"link"}
+                           url={this.props.link}/>
+        <ProductFooterLink type={"github"} url={this.props.github}/>
+        <ProductFooterLink type={"trello"} url={this.props.trello}/>
         {tooltip}
       </div>
     )
@@ -46,8 +23,8 @@ export class ProductFooter extends React.Component {
 }
 
 ProductFooter.propTypes = {
-  is_alive:  Type.bool.isRequired,
-  link:  Type.string,
-  github:  Type.string,
-  trello:  Type.string,
+  is_alive: Type.bool.isRequired,
+  link: Type.string,
+  github: Type.string,
+  trello: Type.string,
 }
