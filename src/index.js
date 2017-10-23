0fc07bd81e2b66efce9d8402e6fb89e238964cf2
elzup <guild0105@gmail.com>
2017-10-24 03:27:20 +0900

    Move React scripts

--- src/index.js
+++ src/index.js
@@ -1,27 +1,25 @@
-import React from "react"
-import { BrowserRouter, Route, Switch } from "react-router-dom"
-import ReactDOM from "react-dom"
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import ReactDOM from "react-dom";
 
-import "babel-polyfill"
+import TopPage from "./pages/top";
+import ProductPage from "./pages/product/index";
+import ArtPage from "./pages/art/index";
+import LogPage from "./pages/log/index";
+import NotFoundPage from "./pages/404/index";
 
-import TopPage from "./pages/top"
-import ProductPage from "./pages/product/index"
-import ArtPage from "./pages/art/index"
-import LogPage from "./pages/log/index"
-import NotFoundPage from "./pages/404/index"
-
-import "./pages/index.css"
+import "./pages/index.css";
 
 // Render the main component into the dom
 ReactDOM.render(
-	<BrowserRouter>
-		<Switch>
-			<Route exact path="/" component={TopPage} />
-			<Route path="/product" component={ProductPage} />
-			<Route path="/art" component={ArtPage} />
-			<Route path="/log" component={LogPage} />
-			<Route component={NotFoundPage} />
-		</Switch>
-	</BrowserRouter>,
-	document.getElementById("container")
-)
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={TopPage} />
+      <Route path="/product" component={ProductPage} />
+      <Route path="/art" component={ArtPage} />
+      <Route path="/log" component={LogPage} />
+      <Route component={NotFoundPage} />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
