2629e2ac7964b011cafbbd23cb8c4c594a9e1138
elzup <guild0105@gmail.com>
2017-10-24 11:22:29 +0900

    StyledC log

--- src/pages/log/index.js
+++ src/pages/log/index.js
@@ -2,18 +2,22 @@
 
 import React from 'react'
 import HeadMenu from '../../components/head-menu'
+import styled from 'styled-components'
 
-export default class LogPage extends React.Component {
+const Stop = styled.div`
+	padding-left: 100px;
+`
+
+export default class LogPage extends React.Component<{}> {
 	render() {
-		const style = require('./log-page.css')
 		return (
-			<main className={style.page}>
+			<main>
 				<header>
 					<HeadMenu current="Log" />
 					<h1>Log</h1>
 					<p>えるざっぷのログ</p>
 				</header>
-				<div className={style.stop}>Under Construction</div>
+				<Stop>Under Construction</Stop>
 			</main>
 		)
 	}
